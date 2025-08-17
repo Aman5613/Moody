52e30362e3ce61b2dbad3261294e4f3a873bd8f8
Aman5613 <aman3779956@gmail.com>
2025-08-17 13:18:54 +0530

    face detection completed

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import FaceExpression from './Components/getExpression'
+
+const App = () => {
+  
+  return (
+    <div>
+      <FaceExpression/>
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/Components/getExpression.jsx
@@ -0,0 +1,56 @@
+import { useRef, useEffect } from "react";
+import * as faceapi from "face-api.js";
+
+export default function FaceExpression() {
+  const videoRef = useRef();
+
+  const loadModels = async () => {
+    const MODEL_URL = `${window.location.origin}/models`;
+    await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+    await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL);
+  };
+
+  const startVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        videoRef.current.srcObject = stream;
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    loadModels().then(startVideo);
+  }, []);
+
+  const handleVideoPlay = async () => {
+    const detections = await faceapi
+      .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+      .withFaceExpressions();
+
+    if (detections.length > 0) {
+      const expressions = detections[0].expressions;
+      const maxExpression = Object.keys(expressions).reduce((a, b) =>
+        expressions[a] > expressions[b] ? a : b
+      );
+      console.log(maxExpression);
+      
+      // console.log("Expressions:", detections[0].expressions);
+
+    } else {
+      console.log("Face not recognized!");
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-5">
+      <video ref={videoRef} autoPlay muted className="h-fit w-100" />
+      <button
+        onClick={handleVideoPlay} 
+        className="h-fit text-white font-bold px-3 py-2 rounded-md bg-teal-800 hover:bg-teal-700 active:scale-95"
+      >
+        Detect Expression
+      </button>
+    </div>
+  );
+}
